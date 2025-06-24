ca442d6b4995179aea5af34ae5086ab39efadabf
Aditya Bose <adi26mar@gmail.com>
2025-06-24 05:53:02 +0530

    feat: create electron app from Next.js app

---
+++ electron/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    }
+  });
+
+  // Load the Next.js dev server or built app
+  const startUrl = isDev 
+    ? 'http://localhost:3000' 
+    : `file://${path.join(__dirname, '../out/index.html')}`;
+  
+  mainWindow.loadURL(startUrl);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
